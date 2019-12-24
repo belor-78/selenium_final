9b152b565b249f255431aff089a43cb78be34158
belor-78 <belor-78@rambler.ru>
2019-12-24 23:52:32 +0200

    начало

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='firefox',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help='Choose language: en, ru, etc')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption('language')
+    browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+        print("\nstart chrome browser for test..")
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        print("\nstart firefox browser for test..")
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    time.sleep(5) # пяти секунд за глаза хватит
+    print("\nquit browser..")
+    browser.quit()
