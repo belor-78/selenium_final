25d89ec85d0b292102dee3c61b29e0c0ceec1d69
belor-78 <belor-78@rambler.ru>
2019-12-25 14:15:07 +0200

    Добавил инициализацию LoginPage через функцию test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -11,6 +11,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_should_be_login_page(browser):
     url = browser.current_url
