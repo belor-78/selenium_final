d0f77dbb9437b45d49674812154ee459b484d6ef
belor-78 <belor-78@rambler.ru>
2019-12-25 14:00:30 +0200

    проверка страницы логина

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def pytest_addoption(parser):
                      help='Choose language: en, ru, etc')
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption('language')
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=3):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
